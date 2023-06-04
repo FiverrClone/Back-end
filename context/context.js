d091d16b7740bfc7b29da522f5948c65ab4ba5c8
Frikh-Said <frikh.said@hotmail.com>
2023-06-04 15:04:27 +0100

    fixing the context

--- context/context.js
+++ context/context.js
@@ -4,14 +4,15 @@ import { getUserFromJWT } from '../helpers/auth.js';
 // import  {GraphQLUpload,graphqlUploadExpress} from 'graphql-upload';
 
 const context = async ({ req ,res }) => {
-  
+  if (req.headers.authorization) {
   // get the user token from the headers
   const token = req.headers.authorization.split(' ')[1] || '';
 
   // try to retrieve a user with the token
   const user = await getUserFromJWT(token);
   // add the user and models to the context
-  return { models,user,req,res};
+  return { models,user,req,res}
+}else return{ models,req,res};
 };
 
 export default context;
